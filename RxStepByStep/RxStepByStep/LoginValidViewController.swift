02eeed4a6fcd3c7d1dbd04bf460deb10e61c99e4
TimberTang <tangming0915@gmail.com>
2017-09-01 15:13:22 +0800

    create login valid view controller

---
+++ RxStepByStep/RxStepByStep/LoginValidViewController.swift
@@ -0,0 +1,29 @@
+
+
+
+//
+//  LoginValidViewController.swift
+//  RxStepByStep
+//
+//  Created by TimberTang on 2017/9/1.
+//  Copyright © 2017年 TimberTang. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class LoginValidViewController: UIViewController {
+
+    @IBOutlet weak var UserNameInput: UITextField!
+    @IBOutlet weak var usernameValidLable: UILabel!
+    @IBOutlet weak var passwordInput: UITextField!
+    @IBOutlet weak var passwordValidLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        let usernameValid = UserNameInput.rx.
+    }
+    
+    @IBOutlet weak var loginAction: UIButton!
+ 
+}
--- RxStepByStep/RxStepByStep/ViewController.swift
+++ RxStepByStep/RxStepByStep/ViewController.swift
@@ -10,16 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func LoginValidAction(_ sender: UIButton) {
+        let loginValid = LoginValidViewController()
+        self.navigationController?.pushViewController(loginValid, animated: true)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
